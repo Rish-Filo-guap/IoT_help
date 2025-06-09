ae32101a3f2948267bb7bdf27527982e3f022069
Rish-Filo-guap <filoliolla@gmail.com>
2025-06-09 21:31:17 +0300

    logger

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
+import threading
+import time
+
 from flask import Flask, render_template, request
 
+import logger
 import things
 
 app = Flask(__name__)
@@ -8,6 +12,22 @@ vacuum=things.Vacuum()
 light=things.Light()
 camera=things.Camera()
 terminal=things.Terminal()
+loger=logger.Log()
+
+
+def log_data():
+    while True:
+        if loger.is_logging:
+            loger.log_data(gripper.get_data(), 'gripper_data')
+            loger.log_data(vacuum.get_data(), 'vacuum_data')
+            loger.log_data(camera.get_data(), 'camera_data')
+        time.sleep(5)
+
+
+thread = threading.Thread(target=log_data)
+thread.daemon=True
+thread.start()
+
 
 
 @app.route('/robot_gripper_connect')
@@ -24,7 +44,14 @@ def r_t_c():
 
 @app.route('/traffic_lights_connect')
 def t_l_c():
-    return light.connect(request)
+    return light.connect()
+
+@app.route('/smart_camera_connect')
+def s_c_c():
+    return camera.connect(request)
+
+
+
 
 
 
