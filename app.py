1e9271ad1baafedfd93d6430d02bcaa46aab88bc
Rish-Filo-guap <filoliolla@gmail.com>
2025-06-09 21:18:20 +0300

    things

--- app.py
+++ app.py
@@ -1,6 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
+import things
 
 app = Flask(__name__)
+gripper=things.Gripper()
+vacuum=things.Vacuum()
+light=things.Light()
+camera=things.Camera()
+terminal=things.Terminal()
+
+
+@app.route('/robot_gripper_connect')
+def r_g_c():
+    return gripper.connect(request)
+
+@app.route('/robot_vacuum_connect')
+def r_v_c():
+    return vacuum.connect(request)
+
+@app.route('/remote_terminal_connect')
+def r_t_c():
+    return terminal.connect(request)
+
+@app.route('/traffic_lights_connect')
+def t_l_c():
+    return light.connect(request)
+
 
 
 @app.route('/')
